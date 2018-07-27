f2622a2a21ad329deb4c7c759a83f8435c107419
BrightestStar <yexx@scmsafe.com>
2018-07-27 11:26:58 +0800

    加入分享，未完成

--- myapp/src/main/java/com/example/myapp/myapp/common/InitializeConfig.java
+++ myapp/src/main/java/com/example/myapp/myapp/common/InitializeConfig.java
@@ -7,6 +7,8 @@ import com.lzy.okgo.cache.CacheEntity;
 import com.lzy.okgo.cache.CacheMode;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
 
 import java.util.logging.Level;
 
@@ -28,6 +30,14 @@ public class InitializeConfig {
             }
 
         });
+
+        UMConfigure.init(appContext,"5a12384aa40fa3551f0001d1"
+                ,"umeng",UMConfigure.DEVICE_TYPE_PHONE,"");//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
+
+        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
 
         //初始化框架
 //        Retrofit.Builder builder = new Retrofit.Builder();
---
+++ myapp/src/main/java/com/example/myapp/myapp/wxapi/WXEntryActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapp.myapp.wxapi;
+
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by daixiankade on 2018/7/26.
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
